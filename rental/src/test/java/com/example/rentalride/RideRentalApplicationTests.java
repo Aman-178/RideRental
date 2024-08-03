28adf640714459f881dce6ec80461d53bdf9175b
aman-178 <amankumar58240@gmail.com>
2024-08-03 17:38:33 +0530

    commit

---
+++ rental/src/test/java/com/example/rentalride/RideRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
