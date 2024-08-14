3b5c8978026e41c7252e49ef9c9485abdaa75d42
aman-178 <amankumar58240@gmail.com>
2024-08-14 17:56:02 +0530

    commit

---
+++ Backend/src/main/java/com/example/rentalride/RideRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideRentalApplication.class, args);
+	}
+
+}
